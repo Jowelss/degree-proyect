e053730e0f823fe931936b0c69a5c22f461cbf8e
Jowelss <devejowels.gitl.loper@gmail.com>
2025-04-04 21:30:13 -0400

    File para crear un nuevo libro

---
+++ src/Libro.jsx
@@ -0,0 +1,11 @@
+function Libro() {
+  return (
+    <div className="libro">
+      <h2>Librito</h2>
+      <p>Autor: SuperJowels</p>
+      <p>Precio: 24bss</p>
+    </div>
+  );
+}
+
+export default Libro;
