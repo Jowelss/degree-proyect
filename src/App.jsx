a51e11a757239108a04b138977e1bcb4400e65dc
Jowelss <devejowels.gitl.loper@gmail.com>
2025-04-04 20:24:49 -0400

    importe la libreria Rutas de react para navegar entre las paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Header from "./Header.jsx";
-import Tienda from "./components/Tienda";
+import Tienda from "./pages/Tienda.jsx";
+import Eventos from "./pages/Eventos.jsx";
+import Reuniones from "./pages/Reuniones.jsx";
 
 function App() {
   return (
-    <>
-      <Header />
-      <Tienda />
-    </>
+    <BrowserRouter>
+      <Header /> {/* Este header es estatico */}
+      <div className="flex justify-center bg-amber-400">
+        <Routes>
+          <Route path="/tienda" element={<Tienda />} />
+          <Route path="/eventos" element={<Eventos />} />
+          <Route path="/reuniones" element={<Reuniones />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
