9f2ea87d791c198acfc62a4b89ed9a598c63a98d
Jowelss <devejowels.gitl.loper@gmail.com>
2025-04-05 18:45:29 -0400

    importe el useState para manejar la apertura y cierre del Modal

--- src/pages/Tienda.jsx
+++ src/pages/Tienda.jsx
@@ -1,46 +1,22 @@
+import { useState } from "react";
+
 import Libro from "../Libro";
 
-function Tienda() {
+function Tienda({ modalAgregarBook }) {
+  const [modal, setModal] = useState(false);
+
+  const handleModal = () => {
+    setModal(!modal);
+  };
+
   return (
     <section className="w-[900px] border">
       <div className="flex justify-end items-center bg-gray-800 text-white p-4 mb-4">
-        {/* mover el formulario a otro archivo  */}
-        <button>Agregar</button>
+        <button onClick={handleModal}>Agregar</button>
       </div>
-      <div>
-        {/* <form>
-          <h1 className="text-4xl font-bold text-gray-800">Tienda</h1>
-          <label htmlFor="">Imagen del libro</label>
-          <input type="file" />
 
-          <label htmlFor="">Nombre del libro</label>
-          <input type="text" />
-
-          <label htmlFor="">Nombre del author</label>
-          <input type="text" />
-
-          <label htmlFor="">Formato</label>
-          <input type="text" />
-
-          <label htmlFor="">Cantidad de paginas</label>
-          <input type="number" />
-
-          <label htmlFor="">Nombre de la editorial</label>
-          <input type="text" />
-
-          <label htmlFor="">Precio</label>
-          <input type="number" />
-
-          <label htmlFor="">Disponibilidad</label>
-
-          <input type="select" />
-
-          <button className="block p-2 bg-blue-500 mt-2 cursor-pointer">
-            Agregar a tienda
-          </button>
-        </form> */}
-        <Libro />
-      </div>
+      <Libro />
+      {modal ? modalAgregarBook : null}
     </section>
   );
 }
