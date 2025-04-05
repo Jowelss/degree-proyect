620198d0dfa01313514cc285c3436e4a505a3a5a
Jowelss <devejowels.gitl.loper@gmail.com>
2025-04-05 18:46:21 -0400

    Estilo flex y dos botones de editar y eliminar

--- src/Libro.jsx
+++ src/Libro.jsx
@@ -1,9 +1,13 @@
 function Libro() {
   return (
-    <div className="libro">
-      <h2>Librito</h2>
-      <p>Autor: SuperJowels</p>
-      <p>Precio: 24bss</p>
+    <div className="flex justify-between p-2.5 bg-amber-200">
+      <h2>EL monje que vendio su ferrari</h2>
+
+      <span>En venta</span>
+      <div className="flex gap-2">
+        <button>Actualizar</button>
+        <button>ELiminar</button>
+      </div>
     </div>
   );
 }
