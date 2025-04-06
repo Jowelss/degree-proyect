290d30722001afaf116fb5b3ba2881b728573c6e
Jowelss <devejowels.gitl.loper@gmail.com>
2025-04-05 21:32:48 -0400

    EL ternario ahora esta fuera del return y envuelto en una variable

--- src/pages/Tienda.jsx
+++ src/pages/Tienda.jsx
@@ -2,21 +2,23 @@ import { useState } from "react";
 
 import Libro from "../Libro";
 
-function Tienda({ modalAgregarBook }) {
-  const [modal, setModal] = useState(false);
+function Tienda({ stateModal }) {
+  const [modalOff, setModal] = useState(false);
+
+  const modal = modalOff ? stateModal : console.log("Sexo");
 
   const handleModal = () => {
-    setModal(!modal);
+    setModal(!modalOff);
   };
 
   return (
-    <section className="w-[900px] border">
+    <section className="w-[900px] border relative">
       <div className="flex justify-end items-center bg-gray-800 text-white p-4 mb-4">
         <button onClick={handleModal}>Agregar</button>
       </div>
 
       <Libro />
-      {modal ? modalAgregarBook : null}
+      {modal}
     </section>
   );
 }
