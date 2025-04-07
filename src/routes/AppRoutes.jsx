1ae0f146b0e57075ccc5550984363035957744d5
Jowelss <devejowels.gitl.loper@gmail.com>
2025-04-07 15:55:19 -0400

    agregue tres parametros

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,20 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Header from "../Header.jsx";
+import Header from '../Header.jsx';
 
-import Tienda from "../pages/Tienda.jsx";
-import Eventos from "../pages/Eventos.jsx";
-import Reuniones from "../pages/Reuniones.jsx";
+import Tienda from '../pages/Tienda.jsx';
+import Eventos from '../pages/Eventos.jsx';
+import Reuniones from '../pages/Reuniones.jsx';
 
-function AppRoutes() {
+function AppRoutes(tienda, eventos, reuniones) {
   return (
     <BrowserRouter>
       <Header />
-      <div className="flex justify-center items-center ">
+      <div className='flex justify-center items-center '>
         <Routes>
-          <Route path="/tienda" element={<Tienda />} />
-          <Route path="/eventos" element={<Eventos />} />
-          <Route path="/reuniones" element={<Reuniones />} />
+          <Route path='/tienda' element={<Tienda />} />
+          <Route path='/eventos' element={<Eventos />} />
+          <Route path='/reuniones' element={<Reuniones />} />
         </Routes>
       </div>
     </BrowserRouter>
