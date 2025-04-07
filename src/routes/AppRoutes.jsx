57701cab0360c9b69af01f066ee94682534477f5
Jowelss <devejowels.gitl.loper@gmail.com>
2025-04-07 16:24:38 -0400

    Cree un objeto agregando los componentes de navegación a cada uno de ellos para que tengan un modal indepedendiente

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -6,13 +6,28 @@ import Tienda from '../pages/Tienda.jsx';
 import Eventos from '../pages/Eventos.jsx';
 import Reuniones from '../pages/Reuniones.jsx';
 
-function AppRoutes(tienda, eventos, reuniones) {
+import { Modal } from '../Modal.jsx';
+
+function AppRoutes() {
+  const dara = {
+    modalTienda: (
+      <Tienda>
+        <Modal />
+      </Tienda>
+    ),
+    modalEvento: (
+      <Eventos>
+        <Modal />
+      </Eventos>
+    ),
+  };
+
   return (
     <BrowserRouter>
       <Header />
       <div className='flex justify-center items-center '>
         <Routes>
-          <Route path='/tienda' element={<Tienda />} />
+          <Route path='/tienda' element={dara.modalTienda} />
           <Route path='/eventos' element={<Eventos />} />
           <Route path='/reuniones' element={<Reuniones />} />
         </Routes>
