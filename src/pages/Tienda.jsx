92a4c874480eafde3a43a71b77aaf13e8f7d3865
Jowelss <devejowels.gitl.loper@gmail.com>
2025-04-07 18:57:21 -0400

    Elimine inputs de form y la importación del Libro

--- src/pages/Tienda.jsx
+++ src/pages/Tienda.jsx
@@ -1,7 +1,4 @@
 import { useState } from 'react';
-
-import Libro from '../Libro';
-
 import { Modal } from '../Modal';
 
 function Tienda() {
@@ -15,36 +12,12 @@ function Tienda() {
         <button onClick={() => setIsOpen(true)}>Abrir Modal</button>
       </div>
 
-      <Libro />
-
       <Modal classState={state} onClosed={() => setIsOpen(false)}>
         <form>
           <h1 className='text-4xl font-bold text-gray-800'>Tienda</h1>
 
           <label>Imagen del libro</label>
-          <input type='file' />
-
-          <label>Nombre del libro</label>
           <input type='text' />
-
-          <label>Nombre del author</label>
-          <input type='text' />
-
-          <label>Formato</label>
-          <input type='text' />
-
-          <label>Cantidad de paginas</label>
-          <input type='number' />
-
-          <label>Nombre de la editorial</label>
-          <input type='text' />
-
-          <label>Precio</label>
-          <input type='number' />
-
-          <label>Disponibilidad</label>
-
-          <input type='select' />
 
           <button className='block p-2 bg-blue-500 mt-2 cursor-pointer'>
             Agregar a tienda
