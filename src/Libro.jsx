1cc94ab7fe121d2b9f6ffcecb8cd9afb950e33b0
Jowelss <devejowels.gitl.loper@gmail.com>
2025-04-06 21:22:33 -0400

    Agregue datos al libro para simular que fueron llenados

--- src/Libro.jsx
+++ src/Libro.jsx
@@ -1,14 +1,33 @@
+const data = [
+  {
+    id: 142343,
+    nombre: "Libro",
+    autor: "Autor",
+    editorial: "Editorial",
+    formato: "Formato",
+    paginas: "Paginas",
+    precio: "24 bs",
+    disponibilidad: "Disponibilidad",
+  },
+];
+
 function Libro() {
   return (
-    <div className="flex justify-between p-2.5 bg-amber-200">
-      <h2>EL monje que vendio su ferrari</h2>
-
-      <span>En venta</span>
-      <div className="flex gap-2">
-        <button>Actualizar</button>
-        <button>ELiminar</button>
-      </div>
-    </div>
+    <article>
+      {data.map((item) => {
+        return (
+          <div key={item.id}>
+            <h2>{item.nombre}</h2>
+            <p>{item.autor}</p>
+            <p>{item.disponibilidad}</p>
+            <p>{item.formato}</p>
+            <p>{item.precio}</p>
+            <p>{item.editorial}</p>
+            <p>{item.paginas}</p>
+          </div>
+        );
+      })}
+    </article>
   );
 }
 
