7210143db1532de1057f6a716c4ce7b021bff330
Jowelss <devejowels.gitl.loper@gmail.com>
2025-04-07 18:20:48 -0400

    Borre esas huevadas

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -6,28 +6,13 @@ import Tienda from '../pages/Tienda.jsx';
 import Eventos from '../pages/Eventos.jsx';
 import Reuniones from '../pages/Reuniones.jsx';
 
-import { Modal } from '../Modal.jsx';
-
 function AppRoutes() {
-  const dara = {
-    modalTienda: (
-      <Tienda>
-        <Modal />
-      </Tienda>
-    ),
-    modalEvento: (
-      <Eventos>
-        <Modal />
-      </Eventos>
-    ),
-  };
-
   return (
     <BrowserRouter>
       <Header />
       <div className='flex justify-center items-center '>
         <Routes>
-          <Route path='/tienda' element={dara.modalTienda} />
+          <Route path='/tienda' element={<Tienda />} />
           <Route path='/eventos' element={<Eventos />} />
           <Route path='/reuniones' element={<Reuniones />} />
         </Routes>
