fa85dc23a825b3eee9a6491460ad57f857632f37
Jowelss <devejowels.gitl.loper@gmail.com>
2025-04-07 16:20:20 -0400

    Elimine el import de Modal y lo agregue como children

--- src/pages/Tienda.jsx
+++ src/pages/Tienda.jsx
@@ -1,25 +1,24 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import { Modal } from "../Modal";
+import Libro from '../Libro';
 
-import Libro from "../Libro";
-
-function Tienda() {
+function Tienda({ children }) {
   const [modalOff, setModal] = useState(false);
 
-  const modal = modalOff ? <Modal /> : console.log("Sexo");
+  const modal = modalOff ? children : console.log('Sexo');
 
   const handleModal = () => {
     setModal(!modalOff);
   };
 
   return (
-    <section className="w-[900px] border relative">
-      <div className="flex justify-end items-center bg-gray-800 text-white p-4 mb-4">
+    <section className='w-[900px] border relative'>
+      <div className='flex justify-end items-center bg-gray-800 text-white p-4 mb-4'>
         <button onClick={handleModal}>Agregar</button>
       </div>
 
       <Libro />
+
       {modal}
     </section>
   );
