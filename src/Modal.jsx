f153d554629a0d43371c66e3b0c12fdb6e74003e
Jowelss <devejowels.gitl.loper@gmail.com>
2025-04-07 16:21:20 -0400

    nada

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,35 +1,36 @@
 export function Modal() {
   return (
-    <article className="border">
+    <article className='border'>
       <button>Cerrar</button>
       <form>
-        <h1 className="text-4xl font-bold text-gray-800">Tienda</h1>
-        <label htmlFor="">Imagen del libro</label>
-        <input type="file" />
+        <h1 className='text-4xl font-bold text-gray-800'>Tienda</h1>
 
-        <label htmlFor="">Nombre del libro</label>
-        <input type="text" />
+        <label>Imagen del libro</label>
+        <input type='file' />
 
-        <label htmlFor="">Nombre del author</label>
-        <input type="text" />
+        <label>Nombre del libro</label>
+        <input type='text' />
 
-        <label htmlFor="">Formato</label>
-        <input type="text" />
+        <label>Nombre del author</label>
+        <input type='text' />
 
-        <label htmlFor="">Cantidad de paginas</label>
-        <input type="number" />
+        <label>Formato</label>
+        <input type='text' />
 
-        <label htmlFor="">Nombre de la editorial</label>
-        <input type="text" />
+        <label>Cantidad de paginas</label>
+        <input type='number' />
 
-        <label htmlFor="">Precio</label>
-        <input type="number" />
+        <label>Nombre de la editorial</label>
+        <input type='text' />
 
-        <label htmlFor="">Disponibilidad</label>
+        <label>Precio</label>
+        <input type='number' />
 
-        <input type="select" />
+        <label>Disponibilidad</label>
 
-        <button className="block p-2 bg-blue-500 mt-2 cursor-pointer">
+        <input type='select' />
+
+        <button className='block p-2 bg-blue-500 mt-2 cursor-pointer'>
           Agregar a tienda
         </button>
       </form>
