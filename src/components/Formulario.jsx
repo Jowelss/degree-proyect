54f8f83146dac92347ec28d69aeadeb6caa71f58
Jowelss <devejowels.gitl.loper@gmail.com>
2025-04-08 16:36:50 -0400

    Cambie el nombre del file a Formulario

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,8 @@
+export function Formulario({ children, classState, onClosed }) {
+  return (
+    <article className={classState}>
+      <button onClick={onClosed}>Cerrar</button>
+      {children}
+    </article>
+  );
+}
