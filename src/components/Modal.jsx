c424e58f80ac3b68a58e5db2314fc3bc114bf8bf
Jowelss <devejowels.gitl.loper@gmail.com>
2025-04-08 16:41:30 -0400

    Archivos con nombres antiguos modificados

--- src/components/Modal.jsx
+++
@@ -1,8 +0,0 @@
-export function Modal({ children, classState, onClosed }) {
-  return (
-    <article className={classState}>
-      <button onClick={onClosed}>Cerrar</button>
-      {children}
-    </article>
-  );
-}
