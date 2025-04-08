53bc502ccb5154ffe3a086885dc502d105afae5f
Jowelss <devejowels.gitl.loper@gmail.com>
2025-04-08 16:36:00 -0400

    cree una funcion para enviar un formulario

--- src/pages/Tienda.jsx
+++ src/pages/Tienda.jsx
@@ -1,10 +1,24 @@
 import { useState } from 'react';
-import { Modal } from '../Modal';
+import { Formulario } from '../components/Formulario.jsx';
 
 function Tienda() {
+  // Metodo para abrir y cerrar el modal
   const [isOpen, setIsOpen] = useState(false);
-
   const state = isOpen ? 'block' : 'hidden';
+  // end
+
+  // Metodo para enviar formulario
+  const [form, setForm] = useState({ nombre: '', author: '', pages: '' });
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Formulario enviado', form);
+  };
+  // end
 
   return (
     <section className='w-[900px] border relative'>
@@ -12,18 +26,42 @@ function Tienda() {
         <button onClick={() => setIsOpen(true)}>Abrir Modal</button>
       </div>
 
-      <Modal classState={state} onClosed={() => setIsOpen(false)}>
-        <form>
+      <Formulario classState={state} onClosed={() => setIsOpen(false)}>
+        <form onSubmit={handleSubmit}>
           <h1 className='text-4xl font-bold text-gray-800'>Tienda</h1>
 
-          <label>Imagen del libro</label>
-          <input type='text' />
+          <label>Nombre del libro</label>
+          <input
+            type='text'
+            name='nombre'
+            value={form.nombre}
+            onChange={handleChange}
+          />
 
-          <button className='block p-2 bg-blue-500 mt-2 cursor-pointer'>
+          <label>Nombre del author</label>
+          <input
+            type='text'
+            name='author'
+            value={form.author}
+            onChange={handleChange}
+          />
+
+          <label>Paginas</label>
+          <input
+            type='number'
+            name='pages'
+            value={form.pages}
+            onChange={handleChange}
+          />
+
+          <button
+            type='submit'
+            className='block p-2 bg-blue-500 mt-2 cursor-pointer'
+          >
             Agregar a tienda
           </button>
         </form>
-      </Modal>
+      </Formulario>
     </section>
   );
 }
