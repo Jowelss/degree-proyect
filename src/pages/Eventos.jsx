d922fca63ff8d829e13f0fe1d803d82481f7befc
Jowelss <devejowels.gitl.loper@gmail.com>
2025-04-08 21:37:57 -0400

    Uso del hook de formulario

--- src/pages/Eventos.jsx
+++ src/pages/Eventos.jsx
@@ -1,10 +1,24 @@
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { Formulario } from '../components/Formulario.jsx';
 
 function Eventos() {
   const [isOpen, setIsOpen] = useState(false);
-
   const state = isOpen ? 'block' : 'hidden';
+
+  // enviar datos del formulario
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+    reset();
+  };
+  // end
 
   return (
     <section className='w-[900px] border'>
@@ -13,14 +27,22 @@ function Eventos() {
       </div>
 
       <Formulario classState={state} onClosed={() => setIsOpen(false)}>
-        <form>
-          <h1 className='text-4xl font-bold text-gray-800'>Tienda</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <h1 className='text-4xl font-bold text-gray-800'>Eventos</h1>
 
           <label>Imagen del Evento</label>
-          <input type='file' />
+          <input
+            type='file'
+            {...register('imagen', { required: 'La imagen es obligatoria' })}
+          />
+          {errors.imagen && <p>{errors.Imagen.message}</p>}
 
           <label>Nombre del Evento</label>
-          <input type='text' />
+          <input
+            type='text'
+            {...register('nombre', { required: 'El nombre es obligatorio' })}
+          />
+          {errors.nombre && <p>{errors.nombre.message}</p>}
 
           <label>Lugar del evento</label>
           <input type='text' />
@@ -28,7 +50,10 @@ function Eventos() {
           <label>Cantidad de integrantes</label>
           <input type='text' />
 
-          <button className='block p-2 bg-blue-500 mt-2 cursor-pointer'>
+          <button
+            type='submit'
+            className='block p-2 bg-blue-500 mt-2 cursor-pointer'
+          >
             Agregar Evento
           </button>
         </form>
