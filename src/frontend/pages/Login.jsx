735c25008bd72073f80e637136c0d2a73b5a7e58
Jowelss <devejowels.gitl.loper@gmail.com>
2025-05-14 18:09:33 -0400

    panel de login con el buttonlogin importado

---
+++ src/frontend/pages/Login.jsx
@@ -0,0 +1,14 @@
+import LoginButton from '../components/LoginButton';
+
+function Login() {
+  return (
+    <div className='h-screen flex items-center justify-center'>
+      <h1 className='size-32'>AUTENTICA</h1>
+      <div className='mr-1.5'>
+        <LoginButton />
+      </div>
+    </div>
+  );
+}
+
+export default Login;
