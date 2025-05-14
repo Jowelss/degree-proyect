e9de4d2786fb899d3eceadc245ae55038259f3f1
Jowelss <devejowels.gitl.loper@gmail.com>
2025-05-13 21:16:52 -0400

    agregue un button con la propiedad de onclick para redireccionar al login de auth0

--- src/frontend/App.jsx
+++ src/frontend/App.jsx
@@ -1,7 +1,8 @@
-import AppRoutes from './routes/AppRoutes.jsx';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
-  return <AppRoutes />;
+  const { loginWithRedirect } = useAuth0();
+  return <button onClick={() => loginWithRedirect()}>Log In</button>;
 }
 
 export default App;
