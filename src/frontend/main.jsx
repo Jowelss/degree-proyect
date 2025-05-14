985c174160a3890f50e9c61c4c34604504e4e299
Jowelss <devejowels.gitl.loper@gmail.com>
2025-05-13 21:15:54 -0400

    importe el auth0 y agregue la funcionalidad para usar el login prediseñado

--- src/frontend/main.jsx
+++ src/frontend/main.jsx
@@ -3,11 +3,20 @@ import { createRoot } from 'react-dom/client';
 import './css/index.css'; //IMPORTACION DEL CSS
 
 import App from './App.jsx'; //IMPORTACION DEL PRIMER COMPONENTE POR DEFECTO
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <Auth0Provider
+    domain='dev-cf87mubjgb6anf57.us.auth0.com'
+    clientId='YkO8HghFwYfmVz1urY7IcQKJBCcojtFr'
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </Auth0Provider>
 );
