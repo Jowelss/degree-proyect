30a7ff791f635481b0e38ea34749951522711d65
Jowelss <devejowels.gitl.loper@gmail.com>
2025-05-14 20:38:14 -0400

    Agregue un titulo para especificar mejor la pagina

--- src/frontend/pages/Login.jsx
+++ src/frontend/pages/Login.jsx
@@ -3,7 +3,7 @@ import LoginButton from '../components/LoginButton';
 function Login() {
   return (
     <div className='h-screen flex items-center justify-center'>
-      <h1 className='size-32'>AUTENTICA</h1>
+      <h1 className='size-32'>ESTE ES LA SECCION DE LOGIN</h1>
       <div className='mr-1.5'>
         <LoginButton />
       </div>
