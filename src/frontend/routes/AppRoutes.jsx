3beab1a4da48720a37b1fa816c7e74ada2449169
Jowelss <devejowels.gitl.loper@gmail.com>
2025-05-15 18:15:47 -0400

    Productos importando para manejar la navegacion

--- src/frontend/routes/AppRoutes.jsx
+++ src/frontend/routes/AppRoutes.jsx
@@ -2,16 +2,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Dashboard from '../pages/dashboard.jsx';
 import Inicio from '../pages/Inicio.jsx';
+import Productos from '../pages/Productos.jsx';
 
 function AppRoutes() {
   return (
     <BrowserRouter>
-      <div>
-        <Routes>
-          <Route path='/inicio' element={<Inicio />} />
-          <Route path='/dashboard' element={<Dashboard />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path='/inicio' element={<Inicio />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/dashboard/productos' element={<Productos />} />
+      </Routes>
     </BrowserRouter>
   );
 }
