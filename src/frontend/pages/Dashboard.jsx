e89b6acee1d6cb3429d58e306e73e2855bf7e466
Jowelss <devejowels.gitl.loper@gmail.com>
2025-05-15 16:29:54 -0400

    Importe el componente Header y agregue elementos tipicos para estructurar la idea

--- src/frontend/pages/Dashboard.jsx
+++ src/frontend/pages/Dashboard.jsx
@@ -1,10 +1,23 @@
-import Header from '../Header.jsx';
+import { Header } from '../components/Header';
+import LogoutButton from '../components/LogoutButton.jsx';
 
 function Dashboard() {
   return (
-    <div className='w-full'>
-      <Header />
-    </div>
+    <Header>
+      <header className='flex justify-around w-full'>
+        <div>
+          <h1>LOGO AUTENTICA</h1>
+        </div>
+        <ul className='flex gap-6'>
+          <li>
+            <h3>tema</h3>
+          </li>
+          <li>
+            <LogoutButton />
+          </li>
+        </ul>
+      </header>
+    </Header>
   );
 }
 
