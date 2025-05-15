5985cce25085abffa833163f2abecfff7e32044d
Jowelss <devejowels.gitl.loper@gmail.com>
2025-05-15 18:13:45 -0400

    Importe el sidebar - comentario.

--- src/frontend/pages/Dashboard.jsx
+++ src/frontend/pages/Dashboard.jsx
@@ -1,23 +1,28 @@
 import { Header } from '../components/Header';
 import LogoutButton from '../components/LogoutButton.jsx';
+import SideBar from '../components/SideBar.jsx';
 
 function Dashboard() {
   return (
-    <Header>
-      <header className='flex justify-around w-full'>
-        <div>
-          <h1>LOGO AUTENTICA</h1>
-        </div>
-        <ul className='flex gap-6'>
-          <li>
-            <h3>tema</h3>
-          </li>
-          <li>
-            <LogoutButton />
-          </li>
-        </ul>
-      </header>
-    </Header>
+    <>
+      <Header>
+        <header className='flex justify-around w-full'>
+          <div>
+            <h1>LOGO AUTENTICA</h1>
+          </div>
+          <ul className='flex gap-6'>
+            <li>
+              <h3>tema</h3>
+            </li>
+            <li>
+              <LogoutButton />
+            </li>
+          </ul>
+        </header>
+      </Header>
+      <SideBar />
+      {/* Aqui ira el contenido dependiendo el sidebar */}
+    </>
   );
 }
 
