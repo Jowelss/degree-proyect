fe045379a44013703df1a3afd072ea13c58512bb
Jowelss <devejowels.gitl.loper@gmail.com>
2025-05-19 22:45:55 -0400

    Le cambie el nombre al file

---
+++ src/backend/index.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import libro from './models/libro.js';
+import cors from 'cors';
+
+const app = express();
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+};
+
+app.use(express.json());
+app.use(cors(corsOptions));
+
+mongoose
+  .connect('mongodb://localhost:27017/autentica', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+
+  .then(() => console.log('Conectado a la base de datos'))
+  .catch((err) => console.log('Error de conexión a la base de datos', err));
+
+app.post('/libros', async (req, res) => {
+  try {
+    const nuevoLibro = new libro(req.body);
+    await nuevoLibro.save();
+
+    res.status(201).send('Libro guardado');
+  } catch (error) {
+    res.status(400).send('Error al guardar el libro');
+
+    console.log(error);
+  }
+});
+
+app.get('/libros', async (req, res) => {
+  const libros = await libro.find();
+  res.json(libros);
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
