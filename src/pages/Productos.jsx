184ba2002eee4d60dfbeac18bdebeac994ba1655
Jowelss <devejowels.gitl.loper@gmail.com>
2025-05-21 18:47:59 -0400

    Agregue una estructura visual de los datos del libro - el onSubmit lo volvi asincrono por si las moscas

--- src/pages/Productos.jsx
+++ src/pages/Productos.jsx
@@ -15,8 +15,8 @@ function Tienda() {
   const { register, handleSubmit, reset } = useForm();
 
   // Funcion con los datos recolectados
-  const onSubmit = handleSubmit((data) => {
-    Add(data, 'libros');
+  const onSubmit = handleSubmit(async (data) => {
+    await Add(data, 'libros');
     reset();
   });
   // end
@@ -24,10 +24,17 @@ function Tienda() {
   return (
     <section className='w-[900px] border relative'>
       <div className='flex justify-end items-center bg-gray-800 text-white p-4 mb-4'>
-        <button onClick={handleClick}>Abrir Modal</button>
+        <button onClick={handleClick}>Agregar Libro</button>
       </div>
 
       <h1 className='text-4xl font-bold text-gray-800'>Productos</h1>
+
+      <ul className='flex justify-around'>
+        <li>Producto</li>
+        <li>Estado</li>
+        <li>Precio</li>
+        <li>Cantidad</li>
+      </ul>
 
       <Formulario classState={state} onClosed={handleClick}>
         <form onSubmit={onSubmit}>
@@ -42,8 +49,8 @@ function Tienda() {
           </div>
 
           <div>
-            <label>Editorial</label>
-            <input type='text' {...register('editorial')} />
+            <label>Género</label>
+            <input type='text' {...register('genero')} />
           </div>
 
           <div>
@@ -57,8 +64,18 @@ function Tienda() {
           </div>
 
           <div>
-            <label>Descripción</label>
-            <input type='text' {...register('descripcion')} />
+            <label>Sinopsis</label>
+            <input type='text' {...register('sinopsis')} />
+          </div>
+
+          <div>
+            <label>Cantidad</label>
+            <input type='number' {...register('cantidad')} />
+          </div>
+
+          <div>
+            <label>Estado</label>
+            <input type='text' {...register('estado')} />
           </div>
 
           <button
