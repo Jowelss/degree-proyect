16a902f51822ef4fc4ccfdb307ad838be41bf88d
Jowelss <devejowels.gitl.loper@gmail.com>
2025-05-21 21:33:55 -0400

    Estilos y modal centrado en la web

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,11 +1,16 @@
 export function Formulario({ children, classState, onClosed }) {
   return (
-    <article className={classState}>
-      <div>
-        <button onClick={onClosed}>Cerrar</button>
-      </div>
+    <article
+      className={`${classState} fixed inset-0 flex justify-center items-center`}
+    >
+      <div className='w-[1000px] bg-gray-200 border rounded-2xl overflow-hidden p-2'>
+        <div className='flex justify-between mb-3'>
+          <span className='text-5xl'>Nuevo libro</span>
+          <button onClick={onClosed}>Cerrar</button>
+        </div>
 
-      <div>{children}</div>
+        <div>{children}</div>
+      </div>
     </article>
   );
 }
