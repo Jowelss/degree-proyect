24b951c8a2e165ce90d221045cf62002f5842085
Jowelss <devejowels.gitl.loper@gmail.com>
2025-05-21 21:36:10 -0400

    Folder database eliminado

--- src/database/Add.jsx
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export async function Add(data, nombre) {
-  try {
-    await axios.post(`http://localhost:5000/${nombre}`, data);
-  } catch (error) {
-    console.log(error);
-  }
-}
