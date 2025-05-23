8b170a928be7124d81f6cbae44e8387a193225e5
Jowelss <devejowels.gitl.loper@gmail.com>
2025-05-23 17:11:00 -0400

    Agregue el trycatch para manejar errores

--- src/services/Get.jsx
+++ src/services/Get.jsx
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
 export async function Get(db) {
-  const response = await axios.get(`http://localhost:5000/${db}`);
-  return response.data;
+  try {
+    const response = await axios.get(`http://localhost:5000/${db}`);
+
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
 }
