35a5474cd2e76aa0ec891c35dc566dd4c2d9090a
Jowelss <devejowels.gitl.loper@gmail.com>
2025-05-26 18:28:20 -0400

    Metodo para eliminar un elemento

--- src/services/Delete.jsx
+++ src/services/Delete.jsx
@@ -1,1 +1,10 @@
-// DE MOMENTO NO TE VOY A USAR PORQUE NO LOGRO LA FORMA DE IMPLEMENTARTE
+import axios from 'axios';
+
+export async function Delete(id, setItem, item, nombre) {
+  try {
+    await axios.delete(`http://localhost:5000/${nombre}/${id}`);
+    setItem(item.filter((item) => item._id !== id));
+  } catch (error) {
+    console.log(`No se puedo eliminar el producto ${error}`);
+  }
+}
