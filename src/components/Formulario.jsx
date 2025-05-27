481d2617332c947329245616ec1fadbe8cc8ec7d
Jowelss <devejowels.gitl.loper@gmail.com>
2025-05-27 16:49:10 -0400

    Quite el button de cerrar y lo volvi un children

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,14 +1,9 @@
-export function Formulario({ children, classState, onClosed }) {
+export function Formulario({ children, classState }) {
   return (
     <article
       className={`${classState} fixed inset-0 flex justify-center items-center`}
     >
       <div className='w-[1000px] bg-gray-200 border rounded-2xl overflow-hidden p-2'>
-        <div className='flex justify-between mb-3'>
-          <span className='text-5xl'>Nuevo libro</span>
-          <button onClick={onClosed}>Cerrar</button>
-        </div>
-
         <div>{children}</div>
       </div>
     </article>
