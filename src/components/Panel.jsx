d0dedf54afad6c04f0daea0153a785805ec0cb8a
Jowelss <devejowels.gitl.loper@gmail.com>
2025-05-28 16:35:32 -0400

    Estilos

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -1,6 +1,6 @@
 export function Panel({ children }) {
   return (
-    <section className='w-[1100px] h-[500px] border rounded-2xl overflow-hidden'>
+    <section className='w-[1100px] h-[500px] rounded-2xl overflow-hidden shadow-[0_0_10px_rgba(0,_0,_0,_0.1)]'>
       {children}
     </section>
   );
