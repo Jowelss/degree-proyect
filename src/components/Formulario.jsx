1166efe187178b9a657b8f425b97bc668839db43
Jowelss <devejowels.gitl.loper@gmail.com>
2025-05-29 17:56:11 -0400

    Estilos

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -3,7 +3,7 @@ export function Formulario({ children, classState }) {
     <article
       className={`${classState} fixed inset-0 flex justify-center items-center bg-[#00000091]`}
     >
-      <div className='w-[800px] bg-white rounded-2xl overflow-hidden p-2'>
+      <div className='w-[900px] bg-white rounded-2xl overflow-hidden p-2'>
         <div>{children}</div>
       </div>
     </article>
