8f09fa2be95690921bb7acf91e661bec711f4c96
Jowelss <devejowels.gitl.loper@gmail.com>
2025-05-29 17:56:51 -0400

    Estilos

--- src/components/LogoutButton.jsx
+++ src/components/LogoutButton.jsx
@@ -3,7 +3,11 @@ import { useAuth0 } from '@auth0/auth0-react';
 function LogoutButton() {
   const { logout } = useAuth0();
 
-  return <button onClick={() => logout()}>Cerrar Sesion</button>;
+  return (
+    <button className='p-1 rounded' onClick={() => logout()}>
+      Cerrar Sesion
+    </button>
+  );
 }
 
 export default LogoutButton;
