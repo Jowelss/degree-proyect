69313620165efa8c4238383e58478b27a5dfd735
Jowelss <devejowels.gitl.loper@gmail.com>
2025-05-29 17:56:28 -0400

    Estilos

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,6 +1,6 @@
 export function ItemCard({ children }) {
   return (
-    <li className='h-18 flex justify-between items-center rounded-2xl overflow-hidden shadow-[0_0_10px_rgba(0,_0,_0,_0.1)] pr-3'>
+    <li className='h-18 flex justify-around items-center text-center rounded-2xl overflow-hidden shadow-[0_0_10px_rgba(0,_0,_0,_0.1)]'>
       {children}
     </li>
   );
