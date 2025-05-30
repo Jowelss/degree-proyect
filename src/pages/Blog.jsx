a2552795aee8a7b6e77a70fee8acc44eb7616a72
Jowelss <devejowels.gitl.loper@gmail.com>
2025-05-30 19:00:18 -0400

    Import de panel

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,5 +1,27 @@
+import { HeaderPanel } from '../components/HeaderPanel';
+import { DataHeader } from '../components/DataHeader';
+import { Panel } from '../components/Panel';
+
 function Blog() {
-  return <h1>FILODEPUTA</h1>;
+  return (
+    <Panel>
+      <HeaderPanel>
+        <h1 className='text-4xl font-bold'>Blog</h1>
+        <button
+          className='p-1.5 rounded-xl'
+          onClick={() => {
+            setSelectId(null);
+            reset();
+            handleClick();
+            changeName('Nuevo libro');
+          }}
+        >
+          Publicar
+        </button>
+      </HeaderPanel>
+      <DataHeader></DataHeader>
+    </Panel>
+  );
 }
 
 export default Blog;
