a61a1c9d700b7c38ced352e03466e937668935b0
Jowelss <devejowels.gitl.loper@gmail.com>
2025-05-30 19:01:14 -0400

    Estilos

--- src/components/DataHeader.jsx
+++ src/components/DataHeader.jsx
@@ -1,6 +1,6 @@
 export function DataHeader({ children }) {
   return (
-    <ul className='flex justify-around mb-4 pr-4 pl-4 text-center shadow-[0px_4px_6px_0px_rgba(0,_0,_0,_0.1)]'>
+    <ul className='flex justify-around mb-4 pr-4 pl-4 text-center shadow'>
       {children}
     </ul>
   );
