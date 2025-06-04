d6e103aa5a7496a5e56659cb8e07e5b093184e3f
Jowelss <devejowels.gitl.loper@gmail.com>
2025-06-04 18:14:43 -0400

    La imagen ahora si se agrega a la base de datos - el problema ahora que cuando envio los datos la casilla de imagen sigue con la imagen anterior

--- src/components/DropImagen.jsx
+++ src/components/DropImagen.jsx
@@ -1,8 +1,16 @@
 import { useDropzone } from 'react-dropzone';
 import axios from 'axios';
+import { useState } from 'react';
 
 export function DropImagen({ setValue }) {
+  const [loading, setLoading] = useState(false);
+  const [isImagen, setImagen] = useState(undefined);
+  const [isAviso, setAviso] = useState('');
+
   const onDrop = async (acceptedFiles) => {
+    setLoading(true);
+    setAviso('hidden');
+
     const formData = new FormData();
     formData.append('file', acceptedFiles[0]);
     formData.append('upload_preset', 'autentica_loveSelf');
@@ -13,21 +21,23 @@ export function DropImagen({ setValue }) {
         formData
       );
 
-      await setValue('imagen', res.data.secure_url);
+      setValue('imagen', res.data.secure_url);
+      setImagen(res.data.secure_url);
     } catch (error) {
       console.log(error);
+    } finally {
+      setLoading(false);
     }
   };
 
   const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
   return (
-    <div
-      {...getRootProps()}
-      className='w-full h-40 flex items-center justify-center border'
-    >
+    <div {...getRootProps()} className='drop-imagen'>
       <input {...getInputProps()} />
-      <span>Arrastra aqui tu imagen</span>
+      <span className={isAviso}>Dale click o arrastra una imagen</span>
+      <img className='object-contain h-full' src={isImagen} />
+      {loading && <span>Cargando imagen</span>}
     </div>
   );
 }
