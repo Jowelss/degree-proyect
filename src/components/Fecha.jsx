2ef8d8df15c2d112b2db06d2750d4846d157b2ef
Jowelss <devejowels.gitl.loper@gmail.com>
2025-06-09 17:47:49 -0400

    Agregue condicionales dentro de useState - agregue un useeffect con una condicional igual - Cree una funcion para el onchange a parte para manejar la carga de la fecha y el envio al setValue

--- src/components/Fecha.jsx
+++ src/components/Fecha.jsx
@@ -1,16 +1,28 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
-export function Fecha({ setValue }) {
-  const [startDate, setStartDate] = useState(new Date());
-  setValue('fecha', startDate.toISOString().split('T')[0]);
+export function Fecha({ setValue, initialDate }) {
+  const [startDate, setStartDate] = useState(
+    initialDate ? new Date(initialDate) : null
+  );
 
+  useEffect(() => {
+    setStartDate(initialDate ? new Date(initialDate) : null);
+  }, [initialDate]);
+
+  const handleChange = (date) => {
+    setStartDate(date);
+
+    setValue('fecha', date);
+  };
   return (
     <DatePicker
       selected={startDate}
-      onChange={(date) => setStartDate(date)}
+      onChange={handleChange}
       dateFormat={'dd/MM/yyyy'}
+      placeholderText='Seleccionar fecha'
     />
   );
 }
