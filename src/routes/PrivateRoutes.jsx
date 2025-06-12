8856efe6f25f4ed701583f882aded425d14a81ff
Jowelss <devejowels.gitl.loper@gmail.com>
2025-06-12 16:52:39 -0400

    Eliminado de momento

--- src/routes/PrivateRoutes.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from 'react-router-dom';
-import { useAuth0 } from '@auth0/auth0-react';
-
-export default function PrivateRoutes({ children }) {
-  const { user } = useAuth0();
-
-  return user ? children : <Navigate to={'/'} />;
-}
