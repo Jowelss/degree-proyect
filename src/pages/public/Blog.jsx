4393ff3d3fb20b932f064a8f9b0acec64bc9d646
Jowelss <devejowels.gitl.loper@gmail.com>
2025-06-17 16:29:57 -0400

    Cree el formulario basico para publicar en el blog, esto ya lo envia a la base de datos, falta las funciones de eliminar de mostrar el post en la ui, el de eliminar y actualizar

--- src/pages/public/Blog.jsx
+++ src/pages/public/Blog.jsx
@@ -1,23 +1,84 @@
-import { Outlet } from 'react-router-dom';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+// Componentes
 import { HeaderPanel } from '../../components/HeaderPanel';
 import { Panel } from '../../components/Panel';
+import { ModalForm } from '../../components/ModalForm';
+// end
 
-import BlogSideBar from '../../components/BlogSideBar';
+// services
+import { Add } from '../../services/Add';
+// end
 
 function Blog() {
+  // Abrir y cerrar modal
+  const [isOpen, setIsOpen] = useState(false);
+  const state = isOpen ? 'block' : 'hidden';
+
+  const handleClick = () => setIsOpen(!isOpen);
+  // end
+
+  const { register, handleSubmit, reset } = useForm();
+
+  const onSubmit = handleSubmit(async (data) => {
+    await Add(data, 'posts');
+    reset();
+  });
+
   return (
     <Panel>
       <HeaderPanel>
         <h1 className='text-4xl font-bold'>Blog</h1>
 
-        <BlogSideBar />
-
-        <button>Publicar</button>
+        <button onClick={handleClick}>Publicar</button>
       </HeaderPanel>
 
-      <div>
-        <Outlet />
-      </div>
+      <ModalForm classState={state} onClosed={handleClick}>
+        <div className='flex justify-between items-center mb-3'>
+          <span className='text-4xl'>Nuevo Post</span>
+          <button onClick={handleClick}>Cerrar</button>
+        </div>
+        <form onSubmit={onSubmit}>
+          <div>
+            <label>Titulo</label>
+            <input type='text' {...register('titulo')} />
+          </div>
+
+          <div>
+            <label>Mensaje</label>
+            <input type='text' {...register('mensaje')} />
+          </div>
+
+          <div>
+            <label>Tipo de publicacion</label>
+
+            <select type='text' {...register('tipo')}>
+              <option value='Ninguno'>Ninguno</option>
+              <option value='Crecimiento personal y emocional'>
+                Crecimiento personal y emocional
+              </option>
+              <option value='Reflexion y frases'>Reflexiones y frases</option>
+              <option value='Mujer autentica y emprendedora'>
+                Mujer autentica y emprendedora
+              </option>
+              <option value='Duelo animal, vinculo ser humano y animal'>
+                Duelo animal, vinculo ser humano y animal
+              </option>
+              <option value='Espacio para adolescentes'>
+                Espacio para adolescentes
+              </option>
+            </select>
+          </div>
+
+          <div>
+            <label>Imagen</label>
+            <input type='text' {...register('imagen')} />
+          </div>
+
+          <button type='submit'>Publicar</button>
+        </form>
+      </ModalForm>
     </Panel>
   );
 }
