79fe787c0d32a79867ef1d952d96708d1c6aa8f0
Jowelss <devejowels.gitl.loper@gmail.com>
2025-06-18 17:59:15 -0400

    Rutas de los blogitems eliminados e igual la ruta al contacto porque ya no se usara

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -6,17 +6,8 @@ import Inicio from '../pages/private/Inicio.jsx';
 import Productos from '../pages/public/Productos.jsx';
 import Eventos from '../pages/public/Eventos.jsx';
 import Blog from '../pages/public/Blog.jsx';
-import Contacto from '../pages/public/Contacto.jsx';
 import Coaching from '../pages/public/Coaching.jsx';
 import Suscripciones from '../pages/public/Suscripciones.jsx';
-
-// Navegacion del blog
-import Crecimiento from '../pages/public/blog-items/Crecimiento.jsx';
-import Adolescentes from '../pages/public/blog-items/Adolescentes.jsx';
-import MujerAutentica from '../pages/public/blog-items/MujerAutentica.jsx';
-import PuroAmor from '../pages/public/blog-items/PuroAmor.jsx';
-import Reflexiones from '../pages/public/blog-items/Reflexiones.jsx';
-// end
 
 function AppRoutes() {
   return useRoutes([
@@ -31,7 +22,6 @@ function AppRoutes() {
         { path: 'blog', element: <Blog /> },
         { path: 'suscripciones', element: <Suscripciones /> },
         { path: 'coaching', element: <Coaching /> },
-        { path: 'contacto', element: <Contacto /> },
       ],
     },
   ]);
