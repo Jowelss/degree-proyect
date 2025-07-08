c690549af339d6e4b41ea4438babaacbe657fae3
Jowelss <devejowels.gitl.loper@gmail.com>
2025-07-08 18:22:31 -0400

    Agregue funcionalidades para que solo muestre el elemento al que se hace click

--- src/pages/TiendaCliente.jsx
+++ src/pages/TiendaCliente.jsx
@@ -5,22 +5,24 @@ import { ModalForm } from '../components/ModalForm';
 import { Get } from '../services/Get.jsx';
 
 function TiendaCliente() {
+  const [selectProduct, setSelectProduct] = useState(null);
+
   const [isOpen, setIsOpen] = useState(false);
   const state = isOpen ? 'block' : 'hidden';
 
-  const handleClick = () => setIsOpen(!isOpen);
+  const handleClick = (item) => {
+    setIsOpen(item);
+    setSelectProduct(item);
+  };
 
   const [producto, setProducto] = useState([]);
 
   const fetchLibros = async () => {
-    // Abrir y cerrar el modal
-
     const data = await Get('libros');
 
     if (data === undefined) {
       console.log('error pa');
     } else {
-      console.log(data);
       setProducto(data);
     }
   };
@@ -30,10 +32,14 @@ function TiendaCliente() {
   }, []);
 
   return (
-    <div className='w-full mt-10'>
+    <div className='w-full flex justify-center gap-4 mt-10'>
       {producto.map((item) => (
-        <ul className='border w-70' onClick={handleClick} key={item._id}>
-          <div className='flex justify-center w-full h-50 mb-1 bg-fuchsia-300'>
+        <ul
+          className='border w-70 cursor-pointer'
+          onClick={() => handleClick(item)}
+          key={item._id}
+        >
+          <div className='flex justify-center w-full h-60 mb-1 bg-fuchsia-300'>
             <img className='object-contain h-full' src={item.imagen} alt='' />
           </div>
           <li className='text-end'>{item.estado}</li>
@@ -43,37 +49,39 @@ function TiendaCliente() {
       ))}
 
       <ModalForm classState={state}>
-        {producto.map((item) => (
-          <div className='max-w-full flex gap-3' key={item._id}>
+        {selectProduct && (
+          <div className='max-w-full flex gap-3' key={selectProduct._id}>
             <div className='min-w-100 h-100'>
               <img
                 className='object-contain h-full'
-                src={item.imagen}
+                src={selectProduct.imagen}
                 alt='Imagen'
               />
             </div>
             <div className='flex-1 overflow-hidden'>
               <div className='text-end'>
-                <button onClick={handleClick}>Cerrar</button>
+                <button onClick={() => handleClick(false)}>Cerrar</button>
               </div>
               <div className='break-words'>
                 <span className='block text-4xl font-extrabold mb-2'>
-                  {item.nombre}
+                  {selectProduct.nombre}
                 </span>
-                <p className='mb-2'>{item.sinopsis}</p>
-                <span>Autor: {item.autor}</span>
+                <p className='mb-2'>{selectProduct.sinopsis}</p>
+                <span>Autor: {selectProduct.autor}</span>
               </div>
               <div className='max-w-max flex gap-2 mb-2'>
-                <span className='border'>{item.tapa}</span>
-                <span className='border'>{item.hoja}</span>
+                <span className='border'>{selectProduct.tapa}</span>
+                <span className='border'>{selectProduct.hoja}</span>
               </div>
               <div>
-                <span className='text-7xl'>{item.precio}</span>
-                <span className='text-2xl'>bs</span>
+                <span className='text-7xl font-bold'>
+                  {selectProduct.precio}
+                </span>
+                <span className='text-2xl font-semibold'>bs</span>
               </div>
             </div>
           </div>
-        ))}
+        )}
       </ModalForm>
     </div>
   );
