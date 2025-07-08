87d4b4bd2bc58b6b76be0b401ffa2fcff0c6cbbb
Jowelss <devejowels.gitl.loper@gmail.com>
2025-07-08 17:45:01 -0400

    Nuevos items para las rutas de blog y eventos

--- src/components/HeaderBar.jsx
+++ src/components/HeaderBar.jsx
@@ -2,9 +2,15 @@ import { Link } from 'react-router-dom';
 
 function Coso() {
   return (
-    <ul>
+    <ul className='flex gap-10 p-2 rounded-2xl border'>
       <li>
         <Link to={'/landing/tiendaCliente'}>Tienda</Link>
+      </li>
+      <li>
+        <Link to={'/landing/blogclient'}>Blog</Link>
+      </li>
+      <li>
+        <Link to={'/landing/eventosclient'}>Eventos</Link>
       </li>
     </ul>
   );
