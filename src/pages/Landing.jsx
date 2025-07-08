9d54885966dc9c12941ed63a389bfe4d82b7aa8a
Jowelss <devejowels.gitl.loper@gmail.com>
2025-07-08 17:44:21 -0400

    Nombre del file coso por HeaderBar - estilos del header principal modificados, navegacion para blog y eventos

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,19 +2,19 @@ import { Outlet } from 'react-router-dom';
 import { Header } from '../components/Header';
 import Logo from '../assets/Autentica2.png';
 import LogoutButton from '../components/LogoutButton';
-import Coso from '../components/Coso';
+import HeaderBar from '../components/HeaderBar';
 
 function Landing() {
   return (
     <>
       <Header>
-        <header className='fixed top-0 h-16 flex justify-between items-center w-full pr-8 pl-8'>
+        <header className='h-16 flex justify-between items-center w-full pr-8 pl-8'>
           <div className='w-14'>
             <img src={Logo} alt='Autentica' />
           </div>
 
           <div>
-            <Coso />
+            <HeaderBar />
           </div>
 
           <ul className='flex gap-6'>
@@ -28,7 +28,7 @@ function Landing() {
         </header>
       </Header>
 
-      <div className='mt-16 h-[calc(100vh-4rem)] w-full pr-4 pl-4'>
+      <div>
         <Outlet />
       </div>
     </>
