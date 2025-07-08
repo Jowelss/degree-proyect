38b6e9d433e1352393cfa1ac94c4dcf42611b6af
Jowelss <devejowels.gitl.loper@gmail.com>
2025-07-08 17:43:05 -0400

    Funciona el abrir el modal, el problema es que cuando todo a un elemento habre todos los elementos del array -  estilos basicos aplicados

--- src/pages/TiendaCliente.jsx
+++ src/pages/TiendaCliente.jsx
@@ -30,9 +30,9 @@ function TiendaCliente() {
   }, []);
 
   return (
-    <div className='border w-70 '>
+    <div className='w-full mt-10'>
       {producto.map((item) => (
-        <ul onClick={handleClick} key={item._id}>
+        <ul className='border w-70' onClick={handleClick} key={item._id}>
           <div className='flex justify-center w-full h-50 mb-1 bg-fuchsia-300'>
             <img className='object-contain h-full' src={item.imagen} alt='' />
           </div>
@@ -42,7 +42,39 @@ function TiendaCliente() {
         </ul>
       ))}
 
-      <ModalForm classState={state}></ModalForm>
+      <ModalForm classState={state}>
+        {producto.map((item) => (
+          <div className='max-w-full flex gap-3' key={item._id}>
+            <div className='min-w-100 h-100'>
+              <img
+                className='object-contain h-full'
+                src={item.imagen}
+                alt='Imagen'
+              />
+            </div>
+            <div className='flex-1 overflow-hidden'>
+              <div className='text-end'>
+                <button onClick={handleClick}>Cerrar</button>
+              </div>
+              <div className='break-words'>
+                <span className='block text-4xl font-extrabold mb-2'>
+                  {item.nombre}
+                </span>
+                <p className='mb-2'>{item.sinopsis}</p>
+                <span>Autor: {item.autor}</span>
+              </div>
+              <div className='max-w-max flex gap-2 mb-2'>
+                <span className='border'>{item.tapa}</span>
+                <span className='border'>{item.hoja}</span>
+              </div>
+              <div>
+                <span className='text-7xl'>{item.precio}</span>
+                <span className='text-2xl'>bs</span>
+              </div>
+            </div>
+          </div>
+        ))}
+      </ModalForm>
     </div>
   );
 }
