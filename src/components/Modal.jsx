404b4f27941131000f0878339f0fc7095becd088
Jowelss <devejowels.gitl.loper@gmail.com>
2025-07-30 15:10:37 -0400

    Ni mrda

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,11 +1,11 @@
 export function Modal({ children, classState }) {
   return (
-    <article
+    <div
       className={`${classState} fixed inset-0 flex justify-center items-center bg-[#00000091]`}
     >
       <div className='w-[900px] bg-white rounded-2xl overflow-hidden p-2'>
         <div>{children}</div>
       </div>
-    </article>
+    </div>
   );
 }
