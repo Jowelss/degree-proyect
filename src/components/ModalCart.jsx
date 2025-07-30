fc1b4fd51122479649934e14509789ece52fc618
Jowelss <devejowels.gitl.loper@gmail.com>
2025-07-29 21:49:16 -0400

    Nuevo file para el modal del carrito

---
+++ src/components/ModalCart.jsx
@@ -0,0 +1,13 @@
+function ModalCart({ classState, children }) {
+  return (
+    <article
+      className={`${classState} fixed inset-0 flex justify-center items-center bg-[#00000091]`}
+    >
+      <div className='w-[400px] bg-white rounded-2xl overflow-hidden p-2'>
+        <div>{children}</div>
+      </div>
+    </article>
+  );
+}
+
+export default ModalCart;
