b0078adf630c1b982cabbb9bef356c91882b243d
Jowelss <devejowels.gitl.loper@gmail.com>
2025-07-30 15:13:51 -0400

    Ni mrda

--- src/components/ModalCart.jsx
+++ src/components/ModalCart.jsx
@@ -1,12 +1,12 @@
 function ModalCart({ classState, children }) {
   return (
-    <article
+    <div
       className={`${classState} fixed inset-0 flex justify-center items-center bg-[#00000091]`}
     >
       <div className='w-[400px] bg-white rounded-2xl overflow-hidden p-2'>
         <div>{children}</div>
       </div>
-    </article>
+    </div>
   );
 }
 
