f7004810c6531ee4fe31496bd50ad64263e06f65
Jowelss <devejowels.gitl.loper@gmail.com>
2025-08-08 21:48:33 -0400

    Amplie el ancho del modal

--- src/components/ModalCart.jsx
+++ src/components/ModalCart.jsx
@@ -3,7 +3,7 @@ function ModalCart({ classState, children }) {
     <div
       className={`${classState} fixed inset-0 flex justify-center items-center bg-[#00000091]`}
     >
-      <div className='w-[400px] bg-white rounded-2xl overflow-hidden p-2'>
+      <div className='w-[600px] bg-white rounded-2xl overflow-hidden p-2'>
         <div>{children}</div>
       </div>
     </div>
