0d56c79259e1eee8abdb5a5ebbb9f95b01356c78
Jowelss <devejowels.gitl.loper@gmail.com>
2025-08-18 16:36:13 -0400

    El authprovider lo movi aqui y ya no esta en el main, me parece que tiene mas sentido que este aca

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import AppRoutes from './routes/AppRoutes';
 import { BrowserRouter } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 function App() {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <Auth0Provider
+      domain={import.meta.env.VITE_DOMAIN_AUTH0}
+      clientId={import.meta.env.VITE_CLIENTID_AUTH0}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </Auth0Provider>
   );
 }
 
