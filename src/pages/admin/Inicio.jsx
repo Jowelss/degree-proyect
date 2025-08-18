a781ad72ecbb588e09e6732df152aaaf2903a92a
Jowelss <devejowels.gitl.loper@gmail.com>
2025-08-18 18:06:39 -0400

    Modifique el import de LoginButton

--- src/pages/admin/Inicio.jsx
+++ src/pages/admin/Inicio.jsx
@@ -1,5 +1,5 @@
 import { Header } from '../../components/Header';
-import LoginButton from '../client/components/LoginButton.jsx';
+import LoginButton from '../../auth/LoginButton.jsx';
 
 function Inicio() {
   return (
