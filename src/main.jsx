dbc256a259a8cc373221a66a1adf446c968acf51
Jowelss <devejowels.gitl.loper@gmail.com>
2025-08-18 16:39:06 -0400

    El provider ahora esta en App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Auth0Provider } from '@auth0/auth0-react';
 
 import './css/index.css'; //IMPORTACION DEL CSS
 import App from './App';
@@ -8,15 +7,7 @@ import App from './App';
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-  <Auth0Provider
-    domain={import.meta.env.VITE_DOMAIN_AUTH0}
-    clientId={import.meta.env.VITE_CLIENTID_AUTH0}
-    authorizationParams={{
-      redirect_uri: window.location.origin,
-    }}
-  >
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </Auth0Provider>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
