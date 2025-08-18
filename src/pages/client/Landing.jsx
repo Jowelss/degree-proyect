20b44fedf438fc8c4e5fb5a3b22f4eb84be04b82
Jowelss <devejowels.gitl.loper@gmail.com>
2025-08-18 16:36:58 -0400

    Importe el Profile para que aparezca en el header de la landing del cliente

---
+++ src/pages/client/Landing.jsx
@@ -0,0 +1,41 @@
+import { Outlet } from 'react-router-dom';
+import Logo from '../../assets/Autentica2.png';
+
+import { Header } from '../../components/Header.jsx';
+import LogoutButton from './components/LogoutButton.jsx';
+import Profile from './components/Profile.jsx';
+import HeaderNav from '../../components/HeaderNav.jsx';
+
+function Landing() {
+  return (
+    <>
+      <Header>
+        <header className='h-16 flex justify-between items-center w-full pr-8 pl-8'>
+          <div className='w-14'>
+            <img src={Logo} alt='Autentica' />
+          </div>
+
+          <div className='flex gap-2 items-center'>
+            <HeaderNav />
+          </div>
+
+          <ul className='flex gap-6'>
+            <li>
+              <h3>tema</h3>
+            </li>
+            <li className='flex'>
+              <LogoutButton />
+              <Profile />
+            </li>
+          </ul>
+        </header>
+      </Header>
+
+      <div>
+        <Outlet />
+      </div>
+    </>
+  );
+}
+
+export default Landing;
