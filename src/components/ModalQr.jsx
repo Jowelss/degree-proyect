1231b42c2a18018b032f565085f819b6df4528f4
Jowelss <devejowels.gitl.loper@gmail.com>
2025-08-25 22:13:34 -0400

    Lo voy a modificar, funciona pero....

--- src/components/ModalQr.jsx
+++ src/components/ModalQr.jsx
@@ -1,6 +1,9 @@
-import QR from '../assets/QR.jpg';
+import { useState } from 'react';
+import { DropImagen } from './DropImagen';
 
 function ModalQr({ classState, coso }) {
+  const [isImagen, setImagen] = useState(null);
+
   return (
     <div
       className={`${classState} fixed inset-0 flex justify-center items-center bg-[#00000091]`}
@@ -8,7 +11,11 @@ function ModalQr({ classState, coso }) {
       <div className='w-[600px] bg-white rounded-2xl overflow-hidden p-2'>
         <button onClick={coso}>Cerrar</button>
         <div className='w-full h-100 flex justify-center'>
-          <img className='object-contain h-full' src={QR} alt='QR' />
+          <DropImagen coso={setImagen}>
+            {isImagen && (
+              <img src={isImagen} alt='Qr' className='object-contain h-full' />
+            )}
+          </DropImagen>
         </div>
 
         <div className='w-full flex justify-center gap-3'>
