74fae3faf736d0ad0e49a4ce3af3a7154abce0a4
Jowelss <devejowels.gitl.loper@gmail.com>
2025-09-01 15:24:57 -0400

    Folder de componentes de admin creada - y movi aqui a dentro el file SideBar

---
+++ src/pages/admin/components/SideBar.jsx
@@ -0,0 +1,27 @@
+import { Link } from 'react-router-dom';
+
+function SideBar() {
+  return (
+    <aside>
+      <ul className='h-[500px] w-[200px] flex gap-2 flex-col justify-around shadow rounded-2xl'>
+        <Link className='sidebar_item' to={'/dashboard/productos'}>
+          Productos
+        </Link>
+        <Link className='sidebar_item' to={'/dashboard/eventos'}>
+          Eventos
+        </Link>
+        <Link className='sidebar_item' to={'/dashboard/blog'}>
+          Blog
+        </Link>
+        <Link className='sidebar_item' to={'/dashboard/suscripciones'}>
+          Suscripciones
+        </Link>
+        <Link className='sidebar_item' to={'/dashboard/sesiones'}>
+          Sesiones
+        </Link>
+      </ul>
+    </aside>
+  );
+}
+
+export default SideBar;
