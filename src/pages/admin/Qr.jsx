8dcae8489079a67820485d0241d3a5f793c5ae4f
Jowelss <devejowels.gitl.loper@gmail.com>
2025-09-02 21:31:54 -0400

    Decidi no separar la funciona de Qr y dropQr, ahora todo se hara en un solo file

--- src/pages/admin/Qr.jsx
+++ src/pages/admin/Qr.jsx
@@ -1,12 +1,45 @@
-import DropImgQr from './components/DropImgQr';
+import { useState, useEffect } from 'react';
+import { useDropzone } from 'react-dropzone';
 
-//Services
+import axios from 'axios';
+
 import { Get } from '../../services/Get';
+import { Add } from '../../services/Add';
 import { Update } from '../../services/Update';
-import { Delete } from '../../services/Delete';
-// end
 
-export default function Qr({ classState, children }) {
+export default function Qr({ children, classState }) {
+  const [qr, setQr] = useState(null);
+
+  const [state, setState] = useState('Agrega una imagen');
+
+  const show = qr ? 'hidden' : 'block';
+
+  const onDrop = async (acceptedFiles) => {
+    setState('Cargando');
+
+    const formData = new FormData();
+    formData.append('file', acceptedFiles[0]);
+    formData.append('upload_preset', 'autentica_loveSelf');
+
+    try {
+      const res = await axios.post(
+        'https://api.cloudinary.com/v1_1/drazdkofq/image/upload',
+        formData
+      );
+
+      setQr(res.data.secure_url);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
+  useEffect(() => {
+    //Si el formulario se cierra el nombre del estado cambia
+    setState('Agrega una imagen');
+  }, [qr]);
+
   return (
     <div
       className={`${classState} fixed inset-0 flex justify-center items-center bg-[#00000091]`}
@@ -14,7 +47,13 @@ export default function Qr({ classState, children }) {
       <div className='w-[500px] bg-white rounded-2xl overflow-hidden p-2'>
         {children}
 
-        <DropImgQr />
+        <div {...getRootProps()} className='drop-imagen cursor-pointer'>
+          <input {...getInputProps()} />
+
+          <span className={show}>{state}</span>
+
+          {qr && <img className='object-contain h-full' src={qr} alt='Qr' />}
+        </div>
 
         <div className='flex justify-around mt-1.5'>
           <button>Guardar</button>
