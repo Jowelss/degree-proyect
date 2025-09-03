a62f62f593fe0d0ea42fb1d234ac9510dff2f9ec
Jowelss <devejowels.gitl.loper@gmail.com>
2025-09-03 17:19:03 -0400

    Agregue el modal de eliminar

--- src/pages/admin/Qr.jsx
+++ src/pages/admin/Qr.jsx
@@ -8,7 +8,12 @@ import { Add } from '../../services/Add';
 import { Update } from '../../services/Update';
 import { Delete } from '../../services/Delete';
 
+import { ModalDelete } from '../../components/ModalDelete.jsx';
+
 export default function Qr({ children, classState, setOpen }) {
+  const [openDelete, setOpenDelete] = useState(false);
+  const handleClickDelete = () => setOpenDelete(!openDelete);
+
   const [qr, setQr] = useState(null);
 
   const [state, setState] = useState('Agrega una imagen');
@@ -72,7 +77,6 @@ export default function Qr({ children, classState, setOpen }) {
     >
       <div className='w-[500px] bg-white rounded-2xl overflow-hidden p-2'>
         {children}
-
         <div {...getRootProps()} className='drop-imagen cursor-pointer'>
           <input {...getInputProps()} />
 
@@ -86,7 +90,6 @@ export default function Qr({ children, classState, setOpen }) {
             />
           )}
         </div>
-
         <div className='flex justify-around mt-1.5'>
           <button
             onClick={() => {
@@ -95,8 +98,26 @@ export default function Qr({ children, classState, setOpen }) {
           >
             Guardar
           </button>
-          <button onClick={deleteQr}>Eliminar</button>
+
+          <button onClick={handleClickDelete}>Eliminar</button>
         </div>
+
+        <ModalDelete classState={openDelete ? 'block' : 'hidden'}>
+          <div className='border p-4 rounded-2xl bg-white'>
+            <h2>¿Estas seguro que quieres eliminar este producto?</h2>
+
+            <div className='flex justify-center gap-2'>
+              <button
+                onClick={() => {
+                  deleteQr(), handleClickDelete();
+                }}
+              >
+                Eliminar
+              </button>
+              <button onClick={handleClickDelete}>Cancelar</button>
+            </div>
+          </div>
+        </ModalDelete>
       </div>
     </div>
   );
