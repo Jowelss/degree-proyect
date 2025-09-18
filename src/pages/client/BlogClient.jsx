72da30a1e2d0b6e41938128af55c1ba866e7ac07
Jowelss <devejowels.gitl.loper@gmail.com>
2025-09-18 18:37:58 -0400

    Easy pa sin explicaciones

--- src/pages/client/BlogClient.jsx
+++ src/pages/client/BlogClient.jsx
@@ -1,5 +1,40 @@
+import { useState, useEffect } from 'react';
+
+import { Get } from '../../services/Get';
+
 function Blog() {
-  return <h1>Blog</h1>;
+  const [post, setPost] = useState([]);
+
+  const getDataPost = async () => {
+    const data = await Get('posts');
+    console.log(data);
+    if (data) {
+      setPost(data);
+    } else {
+      console.log('Algo salio mal wachin');
+    }
+  };
+
+  useEffect(() => {
+    getDataPost();
+  }, []);
+
+  return (
+    <>
+      <div>
+        {post.map((item) => (
+          <ul key={item._id}>
+            <div>
+              <img src={item.imagen} alt='post' />
+            </div>
+            <li>{item.tipo}</li>
+            <li>{item.titulo}</li>
+            <li>{item.mensaje}</li>
+          </ul>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default Blog;
