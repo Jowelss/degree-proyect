b496abbbb1b2deec4672630409fc929a0a78ca23
Jowelss <devejowels.gitl.loper@gmail.com>
2025-09-25 19:07:45 -0400

    Estilos

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -8,6 +8,7 @@ import SideBar from '../admin/components/SideBar.jsx';
 import Logo from '../../assets/Autentica2.png';
 import Qr from './Qr.jsx';
 import { IoQrCode } from 'react-icons/io5';
+import { MdClose } from 'react-icons/md';
 
 function Dashboard() {
   const [isOpenQr, setOpenQr] = useState(false);
@@ -16,13 +17,16 @@ function Dashboard() {
   return (
     <>
       <Header>
-        <header className='fixed top-0 h-16 flex justify-between items-center w-full pr-8 pl-8'>
-          <div className='w-14'>
+        <div className='bg-purple-700 fixed top-0 max-w-[1500px] flex justify-between items-center w-full px-8 py-2'>
+          <div className='w-16 rounded-full bg-white p-2'>
             <img src={Logo} alt='Autentica' />
           </div>
 
-          <div className='cursor-pointer border p-1' onClick={handleClick}>
-            <IoQrCode className='text-black text-3xl' />
+          <div
+            className='cursor-pointer rounded-md bg-white p-1 shadow-[0_0_6px_0_#200035] '
+            onClick={handleClick}
+          >
+            <IoQrCode className='text-purple-950 text-3xl' />
           </div>
 
           <ul className='flex gap-6'>
@@ -33,16 +37,18 @@ function Dashboard() {
               <LogoutButton />
             </li>
           </ul>
-        </header>
+        </div>
       </Header>
 
       <Qr classState={isOpenQr ? 'block' : 'hidden'} setOpen={setOpenQr}>
-        <button className='mb-1.5' onClick={handleClick}>
-          Cerrar
-        </button>
+        <div className='flex justify-end my-3 mr-3'>
+          <button title='Eliminar' onClick={handleClick}>
+            <MdClose className='text-4xl' />
+          </button>
+        </div>
       </Qr>
 
-      <div className='mt-16 h-[calc(100vh-4rem)] flex justify-between items-center w-full pr-4 pl-4'>
+      <div className='mt-16 h-[calc(100vh-4rem)] max-w-[1500px] mx-auto flex justify-between items-center gap-2 w-full pr-4 pl-4'>
         <SideBar />
         <Outlet />
       </div>
