78973f4f3433661bdbea9501109339074655c645
Jowelss <devejowels.gitl.loper@gmail.com>
2025-12-11 21:30:46 -0400

    Ni mrda

---
+++ src/pages/admin/components/Formulario.jsx
@@ -0,0 +1,12 @@
+// import { useForm } from 'react-hook-form
+
+export default function Formulario() {
+  return (
+    <div>
+      <span>
+        Aqui iran todos los formularios del modal pero tengo que ver como
+        agregarlo
+      </span>
+    </div>
+  );
+}
